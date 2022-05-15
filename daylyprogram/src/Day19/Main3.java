b4afb7ec7d7d6b740d0c837037132c844e605e8b
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-05-15 15:06:42 +0900

    HelloWorld1000回出力

--- daylyprogram/src/Day19/Main3.java
+++ daylyprogram/src/Day19/Main3.java
@@ -4,7 +4,6 @@ public class Main3 {
 	public static void main(String[] args) {
 		//1000 個の "Hello World" を出力するプログラムを作成して下さい。
 		for (int i = 1; i <= 1000; i++) {
-			System.out.println(i);
 			System.out.println("Hello World");
 		}
 	}
