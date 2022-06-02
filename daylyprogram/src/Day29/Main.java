89e667f8e9ebea71a52b54cf3b79bcadf43c6f43
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-06-02 23:36:31 +0900

    Day29 commit

---
+++ daylyprogram/src/Day29/Main.java
@@ -0,0 +1,23 @@
+package Day29;
+
+import java.util.Scanner;
+
+//ベクトルと行列の積
+//n * mの行列Aと、m * 1の列ベクトルbを読み込み、Aとbの席を出力するプログラムを作成してください
+//考察
+//行列Aは2次元配列に記録
+//ベクトルbは1次元配列に記録
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		//■Input
+//		1行目にnとmが空白区切りで与えられます
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+//		続くn行に行列Aの要素aijが空白区切りで与えられます。
+//		続くm行にベクトルbの要素biがそれぞれ1行に与えられます。
+		
+		//■Output
+		/* 出力はn行からなります。ベクトルcの要素ciをそれぞれ1行に出力してください。 */
+	}
+}
