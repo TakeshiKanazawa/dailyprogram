fd91570a10996ce0ac923b85ab56200102a6a077
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-06-03 23:12:20 +0900

    Day29 commit

--- daylyprogram/src/Day29/Main.java
+++ daylyprogram/src/Day29/Main.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 //ベクトルと行列の積
 //n * mの行列Aと、m * 1の列ベクトルbを読み込み、Aとbの席を出力するプログラムを作成してください
 //考察
-//行列Aは2次元配列に記録
-//ベクトルbは1次元配列に記録
+//①行列Aは2次元配列に記録
+//②ベクトルbは1次元配列に記録
+//①と②の配列の値を加算して出力
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);		
@@ -14,7 +16,16 @@ public class Main {
 //		1行目にnとmが空白区切りで与えられます
 		int n = sc.nextInt();
 		int m = sc.nextInt();
+		// 多次元配列
+		 int[][]elementArray = new int[n][m];
 //		続くn行に行列Aの要素aijが空白区切りで与えられます。
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				
+				elementArray[i][j] = sc.nextInt();
+			}
+			
+		}
 //		続くm行にベクトルbの要素biがそれぞれ1行に与えられます。
 		
 		//■Output
