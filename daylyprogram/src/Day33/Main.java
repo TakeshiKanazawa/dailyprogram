34359364c413a3c95cd1ea695ba15d0e5baa282d
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-06-13 23:55:26 +0900

    Day33 commit

---
+++ daylyprogram/src/Day33/Main.java
@@ -0,0 +1,19 @@
+package Day33;
+
+import java.util.Scanner;
+
+//大文字と小文字の入れ替え
+//与えられた文字列の小文字と大文字を入れ替えるプログラムを作成してください。
+//■Input
+//文字列が1行に与えられます。
+//■Output
+//与えられた文字列の小文字と大文字を入れ替えた文字列を出力して下さい。アルファベット以外の文字はそのまま出力して下さい。
+//Constraints
+//入力される文字列の長さ < 1200
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String text = sc.next();//
+	}
+}
