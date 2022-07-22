944e40516f10d26efafc95a4d897f89ee5e3a8cb
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-07-22 23:36:29 +0900

    Day38 commit

--- daylyprogram/src/Day38/Main.java
+++ daylyprogram/src/Day38/Main.java
@@ -3,24 +3,30 @@ package Day38;
 import java.util.Scanner;
 //■リング
 
+//■考察
+//文字列Aの中に文字列Bが含まれるかを確認したい。
+//文字ではなく文字列ということがみそ。
+//例えば、Appleの中にpleが含まれているか。
+//しかし、実際には時計回りになっているのでppleaになっていたりする。
+//なので、pplea+ppleaを結合すると、「ppleapplea」となり、indexofで[apple]＝trueとなる
+
+//indexofで比較
+
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		String Ans = "";//出力値初期化
+		String Ans = "Yes";//出力値初期化
+		
 		//■Input
 		// １行目に文字列 sが与えられます。
 		String line_s = sc.next();
 		// 2行目に文字列  pが与えられます。
 		String line_p = sc.next();
-		//■Output
-		//pが作れる場合は Yes と、作れない場合は No と１行に出力してください。
-		for (int i = 0; i < line_p.length(); i++) {
-			String s = line_p;
-			
+		//文字列の連結
+		String connect = line_s.concat(line_s);
+		if(connect.indexOf(line_p) == -1 ) {
+			Ans = "No";
 		}
-		if ( line_s.indexOf(line_p) == -1 ) {
-			System.out.println("No");
-		}
-		else System.out.println("Yes");
+		System.out.println(Ans);
 	}
 }
