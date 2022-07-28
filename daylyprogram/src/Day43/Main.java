dab9cd60ff4f604e657ff5b523a229a0e1303e72
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-07-28 21:57:16 +0900

    Day43 commit

--- daylyprogram/src/Day43/Main.java
+++ daylyprogram/src/Day43/Main.java
@@ -15,41 +15,42 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		// 学生の数 n
+		// 学生の数 n を入力
 		while (true) {
 			int n = sc.nextInt();
 			if (n == 0) {
 				return;
 			}
-			int sum = 0;
-			int score = 0;
-
-			int[] a = new int[n];
+			double score = 0;
+			double averageScore = 0;
+			double[] a = new double[n];
 			// 標準偏差
 			double standardDeviation = 0;
 
 			// データ全体の平均値を出す
 			for (int i = 0; i < n; i++) {
-				int s = sc.nextInt();
+				double s = sc.nextDouble();
 				a[i] = s;
+				//合計スコアに加算
 				score += s;
 			}
-			score = score / n;
+			//合計スコアを人数で除算し平均値を求めて変数に代入
+			averageScore = score / n;
 
 			// .偏差（各データから平均値を差し引いた値）を求める
 			for (int i = 0; i < n; i++) {
-				a[i] = a[i] - score;
+				a[i] = a[i] - averageScore;
 				// 算出した偏差を2乗する
 				a[i] = a[i] * a[i];
 			}
 			// 偏差の合計を出す
-			int sum2 = Arrays.stream(a).sum();
+			double sumstandardDeviation = Arrays.stream(a).sum();
 			// 偏差の合計をデータの総数で割って分散を求める
-			sum2 = sum2 / n;
-
+			double dispersion = (double)sumstandardDeviation / n;
 			// 分散の正の平方根を求めて標準偏差を算出する
-			standardDeviation = Math.sqrt(sum2);
-			System.out.println(standardDeviation);
+			standardDeviation = Math.sqrt(dispersion);
+//			System.out.println(standardDeviation);
+			System.out.println(String.format("%.8f", standardDeviation));
 		}
 	}
 }
