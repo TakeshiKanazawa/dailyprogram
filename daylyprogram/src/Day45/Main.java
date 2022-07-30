ca63823d5ee19e65485c538d5d588d3eb96a100e
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-07-30 22:46:29 +0900

    Day45 commit

---
+++ daylyprogram/src/Day45/Main.java
@@ -0,0 +1,32 @@
+package Day45;
+
+import java.util.Scanner;
+
+//サイコロのクラス
+class Dice {
+	
+	// コンストラクタ
+	
+	// サイコロを転がす
+	void move() {
+		
+	}
+	
+	// サイコロの上面の数を出力
+	void print() {
+		
+	}
+	
+}
+
+//サイコロ I
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// １行目に各面の整数が、図に示すラベルの順番に空白区切りで与えられます。
+		
+		// ２行目に命令を表す１つの文字列が与えられます。命令はそれぞれ図に示す４方向を表す文字 E、N、S、W を含む文字列です。
+		
+	}
+}
