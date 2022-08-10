f383ee3541ba53c1786776a6fc8d0be1388a8985
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-08-10 22:51:11 +0900

    Day48 commit

--- daylyprogram/src/Day48/Main2.java
+++ daylyprogram/src/Day48/Main2.java
@@ -2,8 +2,11 @@ package Day48;
 
 import java.util.Scanner;
 
-public class Main {
+// 最高得点を出力する
+public class Main2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		int n =  sc.nextInt();
+		System.out.println(100 + n * 10);
 	}
 }
