a5b49771bcf4b83cf16b15d4c4d6a54b6d4194c8
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-08-15 23:31:02 +0900

    Day51 commit

---
+++ daylyprogram/src/Day51/Main.java
@@ -0,0 +1,42 @@
+package Day51;
+
+// B - Papers, Please
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String Ans = "";
+		// 整数Nの数
+		int N = sc.nextInt();
+		// N個の整数
+		for (int i = 0; i < N; i++) {
+			int seisu = sc.nextInt();
+			if (isEven(seisu)) {
+				boolean w = isWareru(seisu);
+				if (w) {
+					Ans = "APPROVED";
+				} else if (!w) {
+					Ans = "DENIED";
+					System.out.println(Ans);
+					return;
+				}
+			}
+		}
+		System.out.println(Ans);
+	}
+
+	// 偶数判定メソッド
+	static boolean isEven(int n) {
+		return n % 2 == 0;
+	}
+
+	static boolean isWareru(int n) {
+		if (n % 3 != 0 && n % 5 != 0) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+}
