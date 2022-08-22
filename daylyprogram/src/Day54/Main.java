93a525fb39e7e6a075f3ecd489598182561a3dca
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-08-22 21:55:10 +0900

    Day54

--- daylyprogram/src/Day54/Main.java
+++ daylyprogram/src/Day54/Main.java
@@ -5,9 +5,13 @@ package Day54;
 //ただしアルファベット順で Z の 1 個後の文字は A とみなします。
 
 //■考察
-//入力文字列分のchar型の配列を作成
-//まず文字列を先頭から1もじずつ取得しchar型に変換して格納
-//与えられた整数Nの部分だけ数字を加算する
+//まずアルファベット全文字を配列に格納
+//入力文字列を配列に格納
+//アルファベット文字数分ループを回すし、入力文字列から1もじづつ取り出し、
+//合致する文字があれば配列に格納。
+//その際、Nを足した配列の文字を格納する。
+//もし、Nをたした数が26以下でなければ
+//26いかになるまで26を引く= ループさせる(while = true)
 
 //出力を行う
 
@@ -16,20 +20,37 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
-		int N = sc.nextInt();
-		String S = sc.next();
-		//入力文字列分のchar型の配列を作成
-	    
-	    char []alphabet = new char[26];
-	    char c = 'a';
-	    for (int i = 0; i < 26; i++){
-	        alphabet[i] = c++;
-	        System.out.println(alphabet[i+N]);
-	    }
-	    
-	    for (int i = 0; i < S.length(); i++){
-	        System.out.println(alphabet[i]);
-	    }
-	  }
+
+		int N = sc.nextInt();// 整数N
+		String S = sc.next();// 文字列S
+		// まずアルファベット全文字を配列に格納
+		char[] alphabet = new char[26];
+		char c = 'A';
+		for (int i = 0; i < 26; i++) {
+			alphabet[i] = c++;
+		}
+		// 入力文字列を配列に格納
+		char[] inputArr = new char[S.length()];
+		for (int i = 0; i < S.length(); i++) {
+			inputArr[i] = S.charAt(i);
+		}
+
+		// アルファベット文字数分ループを回すし、入力文字列から1もじづつ取り出し、合致する文字があれば配列に格納。
+		for (int i = 0; i < inputArr.length; i++) {
+			for (int j = 0; j < alphabet.length; j++) {
+				if (inputArr[i] == alphabet[j]) {
+					int num = j + N;
+					// //もし、Nをたした数が26以下でなければ
+					// 26いかになるまで26を
+					if (num >= 26) {
+						while (num >= 26) {
+							num -= 26;
+						}
+					}
+					System.out.print(alphabet[num]);
+					break;
+				}
+			}
+		}
 	}
+}
