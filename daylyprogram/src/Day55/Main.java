e582af46181e2789e661013c361a6f9399260acf
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-08-23 22:54:08 +0900

    Day55

---
+++ daylyprogram/src/Day55/Main.java
@@ -0,0 +1,14 @@
+package Day55;
+
+//高橋君は九九を覚えたので、1 以上 9 以下の 2 つの整数の積を計算することができます。
+//
+//整数 N が与えられるので、N を 1 以上 9 以下の 2 つの整数の積として表すことができるか判定し、
+//できるなら Yes を、できないなら No を出力して下さい。
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
