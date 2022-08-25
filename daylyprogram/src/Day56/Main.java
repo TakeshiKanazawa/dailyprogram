d5b70afb5d932a84c207faf4bb82a79bc5de8c9e
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-08-25 23:13:47 +0900

    Day56

---
+++ daylyprogram/src/Day56/Main.java
@@ -0,0 +1,14 @@
+package Day56;
+
+//高橋君はデータの加工が行いたいです。
+//整数 a,b,cと、文字列 s が与えられます。 a+b+c の計算結果と、文字列 s を並べて表示しなさい。
+//
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
