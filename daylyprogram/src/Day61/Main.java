513e3eff6d3d2710e160e47957b2baed48230eba
TakeshiKanazawa <oseans@hotmail.co.jp>
2022-09-20 23:49:08 +0900

    Day61 commit

---
+++ daylyprogram/src/Day61/Main.java
@@ -0,0 +1,20 @@
+package Day61;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		// 入力
+		int N = sc.nextInt();
+		
+		//aがbで割り切れるとき: a/b
+		//aがbで割り切れないとき: a/b + 1
+		
+		
+			System.out.println((N + 2 -1)/2);
+		
+		// (a + b - 1) / b
+	}
+}
